1cb5283c13689f6dbac83ff3e479c91dd0b22491
shawn10067 <shawn10067@gmail.com>
2022-07-18 18:09:16 -0400

    finished creating adaptive themed button

---
+++ src/components/RoundedButton.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Pressable } from "react-native";
+import styled from "styled-components/native";
+import theme from "../infrastructure/theme";
+
+const RoundedButton = ({
+  colour = "green",
+  text,
+  fontSize = 40,
+  normalBackgroundColor = "white",
+  pressedBackgroundColor = "#C9C5C6",
+  ...props
+}) => {
+  // getting the pressed background colour based on if its in the pallate or not
+  const pressedBG = theme.colours.main[pressedBackgroundColor]
+    ? theme.colours.main[pressedBackgroundColor]
+    : pressedBackgroundColor;
+
+  // getting the normal background colour based on if its in the pallate or not
+  const normalBG = theme.colours.main[normalBackgroundColor]
+    ? theme.colours.main[normalBackgroundColor]
+    : normalBackgroundColor;
+
+  // button styling
+  const Button = styled(Pressable).attrs()`
+    border-color: ${({ theme }) =>
+      theme.colours.main[colour] ? theme.colours.main[colour] : colour};
+    border-radius: 30px;
+    border-width: 4px;
+    background-color: ${normalBG};
+    margin: 10px;
+    height: 70px;
+    width: 220px;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  // text styling
+  const Text = styled.Text`
+    font-family: FuturaPT;
+    font-size: ${fontSize}px;
+    color: ${({ theme }) =>
+      theme.colours.main[colour] ? theme.colours.main[colour] : colour};
+  `;
+
+  // passing text in the pressable
+  return (
+    <Button
+      {...props}
+      style={({ pressed }) => [
+        pressed && { backgroundColor: pressedBG },
+        props.style,
+      ]}
+    >
+      <Text>{text}</Text>
+    </Button>
+  );
+};
+
+export default RoundedButton;
--- src/features/home/screens/home.screen.js
+++ src/features/home/screens/home.screen.js
@@ -2,11 +2,13 @@ import React from "react";
 import styled from "styled-components/native";
 import BackgroundView from "../components/BackgroundView";
 import SafeAreaView from "../../../components/SafeAreaView";
+import RoundedButton from "../../../components/RoundedButton";
 
 const TextView = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  width: 100%;
 `;
 
 const MainText = styled.Text`
@@ -15,12 +17,23 @@ const MainText = styled.Text`
   font-family: FuturaPT;
 `;
 
+const WideRoundButton = styled(RoundedButton)`
+  width: 30%;
+  height: 50px;
+`;
+
 const HomeScreen = () => {
   return (
     <BackgroundView>
       <SafeAreaView>
         <TextView>
           <MainText>Email</MainText>
+          <WideRoundButton
+            text="W"
+            colour="blue"
+            onPress={() => console.log("Sent")}
+            fontSize={30}
+          />
         </TextView>
       </SafeAreaView>
     </BackgroundView>
