c5c0aec3d3bd07c5cf6bf60273db1b69c4da566f
GurshawnL <58784041+GurshawnL@users.noreply.github.com>
2022-07-30 04:30:04 -0400

    started view properties screen

---
+++ src/features/home/screens/view-properties.screen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BackgroundView from "../../../components/BackgroundView";
+import SafeAreaView from "../../../components/SafeAreaView";
+
+const ViewPropertiesScreen = ({ navigation }) => {
+  return (
+    <BackgroundView>
+      <SafeAreaView></SafeAreaView>
+    </BackgroundView>
+  );
+};
+
+export default ViewPropertiesScreen;
